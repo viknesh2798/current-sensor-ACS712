5280ed25829e11060f0b216a67124ec39f224026
Viknesh <35456645+viknesh2798@users.noreply.github.com>
2020-04-14 22:56:21 +0530

    Update MultipleCurrentSensorMeasurement.ino

--- examples/MultipleCurrentSensorMeasurement/MultipleCurrentSensorMeasurement.ino
+++ examples/MultipleCurrentSensorMeasurement/MultipleCurrentSensorMeasurement.ino
@@ -8,7 +8,7 @@ ACS712 sensor2(ACS712_30A, A1);
 void setup() {
   Serial.begin(9600);
 
-  // This method calibrates zero point of  each sensor,
+  // This method calibrates zero point of each sensor,
   // It is not necessary, but may positively affect the accuracy
   // Ensure that no current flows through the sensor at this moment
   sensor1.calibrate();
@@ -17,11 +17,13 @@ void setup() {
 
 void loop() {
   
-  // To measure current we need to know the frequency of current
+  // To measure current we need to know the frequency of current.
   // By default 50Hz is used, but you can specify own, if necessary
   float i1 = sensor1.getCurrentAC();
   float i2 = sensor2.getCurrentAC();
-
+  
+  // The current through each current sensor is printed 
+  // every 1 sec in the Serial monitor
   Serial.println(String("I1 = ") + i1 + " A" + "\tI2 = " + i2 + " A");
-  delay(1000);
+  delay(1000); 
 }
