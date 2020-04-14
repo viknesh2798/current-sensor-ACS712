e8401602fe0b289d59bc64dd11dabfdf43bba8b3
Viknesh <35456645+viknesh2798@users.noreply.github.com>
2020-04-14 22:44:24 +0530

    Add files via upload

---
+++ examples/MultipleCurrentSensorMeasurement/MultipleCurrentSensorMeasurement.ino
@@ -0,0 +1,27 @@
+#include "ACS712.h"
+
+// We have 30 amps version sensor connected to A0 and A1 pin of arduino
+// Replace with your version if necessary
+ACS712 sensor1(ACS712_30A, A0);
+ACS712 sensor2(ACS712_30A, A1);
+
+void setup() {
+  Serial.begin(9600);
+
+  // This method calibrates zero point of  each sensor,
+  // It is not necessary, but may positively affect the accuracy
+  // Ensure that no current flows through the sensor at this moment
+  sensor1.calibrate();
+  sensor2.calibrate();
+}
+
+void loop() {
+  
+  // To measure current we need to know the frequency of current
+  // By default 50Hz is used, but you can specify own, if necessary
+  float i1 = sensor1.getCurrentAC();
+  float i2 = sensor2.getCurrentAC();
+
+  Serial.println(String("I1 = ") + i1 + " A" + "\tI2 = " + i2 + " A");
+  delay(1000);
+}
